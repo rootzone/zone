d74c62ef5d31722515b0bde6ab1d0100a311ee65
Root Zone <rootzone@devnull>
2018-01-29 13:27:19 +0000

    -

--- fetch.sh
+++ fetch.sh
@@ -5,6 +5,6 @@ git diff-index --quiet HEAD -- root.zone
 if [ $? -eq 1 ]
 then
 	git add root.zone 
-	git commit -m'add new copy of root zone' root.zone
+	git commit -a -m'add new copy of root zone'
 	git push
 fi
