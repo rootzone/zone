56cadc24ce80b877376e7e66abd065020e578f9d
Root Zone <rootzone@devnull>
2018-01-29 13:32:46 +0000

    test

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 PATH=/usr/bin
 wget -qO root.zone http://www.internic.net/domain/root.zone
-git diff-index --quiet HEAD -- root.zone
+#git diff-index --quiet HEAD -- root.zone
+git diff --quiet HEAD -- root.zone
 if [ $? -eq 1 ]
 then
 	git add root.zone 
