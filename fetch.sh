c02f721f43a867099f0b82b25293c345f89d3b86
Root Zone <rootzone@devnull>
2018-01-29 12:41:13 +0000

    update script

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PATH=/usr/bin
 wget http://www.internic.net/domain/root.zone
-git diff-index --quiet HEAD -- || git add root.zone && git commit -m'add new copy of root zone' && git push
+git diff-index --quiet HEAD -- || git add --quite root.zone && git commit --quite  -m'add new copy of root zone' && git push --quite
