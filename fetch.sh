0077c5ec4bd6bed3baf6478ff6b9bd33c3cfabed
Root Zone <rootzone@devnull>
2018-01-30 11:47:22 +0000

    update fetch script

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 PATH=/usr/bin
 cd "$(dirname "$0")"
-wget -qO root.zone http://www.internic.net/domain/root.zone
+#wget -qO root.zone http://www.internic.net/domain/root.zone
+dig AXFR . @xfr.dns.icann.org | awk '$4!="RRSIG" && $1!=";;" {print}' > root.zone
 #git diff-index --quiet HEAD -- root.zone
 git diff --quiet HEAD -- root.zone
 if [ $? -eq 1 ]
